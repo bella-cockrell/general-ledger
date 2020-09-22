86abb517facaac94c7b9e1e4cc5d9d08bcd8277a
Claire Guest <claire.guest@madetech.com>
2020-09-22 12:10:22 +0100

    Add name instance variable

--- journal.rb
+++ journal.rb
@@ -1,8 +1,8 @@
 class Journal
-    attr_accessor :list_of_transactions
+    attr_accessor :list_of_transactions, :name
     
     def initialize(journal_name)
-        # @name = journal_name
+        @name = journal_name
         @list_of_transactions = []
     end
 
